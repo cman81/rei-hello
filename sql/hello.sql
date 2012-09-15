6e7a5e1e65b7b03c046678a829714b6af0c54a4c
zerihun <zerihunz@gmail.com>
2012-09-15 18:54:31 -0400

    Added more data to the database.

--- sql/hello.sql
+++ sql/hello.sql
@@ -30,11 +30,11 @@ CREATE TABLE `employees` (
   `mentor_id` int(6) NOT NULL,
   `prog_mgr_id` int(6) NOT NULL,
   `resource_mgr_id` int(6) NOT NULL,
-  `proj_mgr_id` int(6) NOT NULL,
+  `proj_id` int(6) NOT NULL,
   `user_id` int(6) NOT NULL,
   `created_on` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +43,33 @@ CREATE TABLE `employees` (
 
 LOCK TABLES `employees` WRITE;
 /*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES (1,'Kapil','Grover','Senior Graphics Designer',3,4,3,1,2,'0000-00-00'),(2,'David','Rhyne','Graphics Designer',2,4,3,2,1,'0000-00-00'),(3,'Christian','Manalansan','Senior Software Engineer',5,4,7,2,6,'0000-00-00'),(4,'Darren','Harper','Senior Graphics Designer',2,4,7,2,3,'0000-00-00'),(5,'Amit','Yadav','President',0,4,0,0,4,'0000-00-00'),(6,'Zerihun','Tegegn','Senior Software Engineer',6,4,7,1,5,'0000-00-00'),(7,'Tim','Molinaro','Resource Manager',4,4,4,2,7,'0000-00-00'),(8,'Sudheendra','Mahdur','Project Manager',4,4,4,1,8,'0000-00-00');
 /*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects`
+--
+
+DROP TABLE IF EXISTS `projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `projects` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `manager` int(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projects`
+--
+
+LOCK TABLES `projects` WRITE;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+INSERT INTO `projects` VALUES (1,'Performance.gov',8),(2,'Shared Services',7),(3,'',0);
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -60,7 +86,7 @@ CREATE TABLE `users` (
   `role` varchar(50) NOT NULL,
   `created_on` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +95,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'david','123','emp','0000-00-00'),(2,'kapil','123','emp','0000-00-00');
+INSERT INTO `users` VALUES (1,'drhyne@reisystems.com','123','emp','0000-00-00'),(2,'kgrover@reisystems.com','123','emp','0000-00-00'),(3,'dharper@reisystems.com','123','emp','0000-00-00'),(4,'ayadav@reisystems.com','123','emp','0000-00-00'),(5,'ztegegn@reisystems.com','123','emp','0000-00-00'),(6,'cmanalansan@reisystems.com','123','emp','0000-00-00'),(7,'tmolinaro@reisystems.com','123','emp','0000-00-00'),(8,'smadhur@reisystems.com','123','emp','0000-00-00');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -82,4 +108,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-09-15  2:18:11
+-- Dump completed on 2012-09-15 18:47:07
