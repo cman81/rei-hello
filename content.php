ffb6b3686633e34f8abae4904161699bdb8632c5
zerihun <zerihunz@gmail.com>
2012-09-15 19:56:02 -0400

    First Commit

---
+++ content.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ztegegn
+ * Date: 9/15/12
+ * Time: 6:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+echo "from content";
+?>
