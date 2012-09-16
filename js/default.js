f59cf18e2d58bc10d17ee5b8962767ce79521f74
Michael Ton <mton@reisystems.com>
2012-09-16 15:48:18 -0400

    nav theming/functionality

--- js/default.js
+++ js/default.js
@@ -15,24 +15,27 @@ $(document).ready(function() {
 
 	var latestStep = 0;
 
+	// When user clicks on "Continue" button
   $('.continue').click(function(e) {
-  	e.preventDefault();
 
   	var currentStep = $('.step.active').attr('id').substr(-1);
   	var nextStep = (parseInt(currentStep)+1).toString();
 
   	$('#step-'+currentStep).removeClass('active')
-  		.addClass('completed')
+  		.addClass('complete')
   		.hide("slide", { direction: "left" }, 200);
   	$('#nav_'+currentStep).removeClass('active')
-  		.addClass('completed');
+  		.addClass('complete');
   	$('#step-'+nextStep).addClass('active')
   		.fadeIn(600);
 		$('#nav_'+nextStep).addClass('active');
 
 	if (nextStep > latestStep) latestStep = nextStep;
+
+	e.preventDefault();
   });
 
+  // When user clicks on a navigation button
   $('.nav-container').click(function(e) {
   	var clickedStep = $(this).attr('id').substr(-1);
   	if (clickedStep < latestStep) {
@@ -81,6 +84,7 @@ function switchStep(step) {
   			.fadeIn(600);
   	},400);
 	$('#nav_'+step).addClass('active');
+}
 
 function setSelectionRange(input, selectionStart, selectionEnd) {
 	if (input.setSelectionRange) {
